697df7fc4a117c18555ff5a5334ac41fd2704f6d
Rdemora2 <robertomoraeszar@gmail.com>
2024-02-06 17:57:04 -0300

    FEAT/ versão inicial de fluxo de logout

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,8 +3,11 @@
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
         <v-toolbar-title>Bem-vindo à sua aplicação!</v-toolbar-title>
         <v-spacer></v-spacer>
+
         <v-btn v-if="!isAuthenticated" to="/register" color="primary">Registrar</v-btn>
         <v-btn v-if="!isAuthenticated" to="/login" color="success">Login</v-btn>
+
+        <v-btn v-if="isAuthenticated" @click="logout" color="error">Logout</v-btn>
     </v-app-bar>
 </template>
   
@@ -18,12 +21,22 @@ export default {
         },
     },
     async created() {
-        if (this.isAuthenticated) {
-            try {
-                await validateToken(this.token);
-            } catch (error) {
+        await this.checkAuthentication();
+    },
+    methods: {
+        async checkAuthentication() {
+            if (this.isAuthenticated) {
+                try {
+                    await validateToken(this.token);
+                } catch (error) {
+                    this.$store.dispatch('clearToken');
+                }
             }
-        }
+        },
+        async logout() {
+            this.$store.dispatch('clearToken');
+            this.checkAuthentication();
+        },
     },
 };
 </script>
