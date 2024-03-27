d315b01e011c265980d0174e42fa615d1d9ee0a1
Rdemora2 <robertomoraeszar@gmail.com>
2024-03-27 17:02:23 -0300

    TEST/ cria primeiro teste unitario

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
     testEnvironment: 'node',
     coverageThreshold: {
         global: {
-            branches: 80,
-            functions: 80,
             lines: 80,
             statements: 80,
         },
